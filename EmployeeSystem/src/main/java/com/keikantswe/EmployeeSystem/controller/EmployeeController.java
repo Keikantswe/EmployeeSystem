3f8f0e36843d2c6ac7d5d4c658d4bfd238f9b5ca
Keikantswe More <113474880+Keikantswe@users.noreply.github.com>
2023-12-01 11:47:42 +0200

    Update EmployeeController.java

--- EmployeeSystem/src/main/java/com/keikantswe/EmployeeSystem/controller/EmployeeController.java
+++ EmployeeSystem/src/main/java/com/keikantswe/EmployeeSystem/controller/EmployeeController.java
@@ -6,7 +6,7 @@ import com.keikantswe.EmployeeSystem.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-@CrossOrigin(origins = "http://localhost:3000")
+//@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/api/v1/")
 public class EmployeeController {
